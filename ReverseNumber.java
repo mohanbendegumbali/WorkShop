7fcdb994e10e6da613fe47fb508cb4f073e5ede6
mohanbendegumbali <mohan0311@hotmail.com>
2023-01-13 22:33:15 +0530

    Added Reverse Number

---
+++ ReverseNumber.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.workshop;
+
+public class ReverseNumber {
+       public static void main(String args[]){
+           int num = 12345;
+           int rev =0;
+           while(num!=0){
+               int rem = num % 10;
+               rev = rev * 10 + rem;
+               num = num/10;
+           }
+           System.out.println(" Reverse of Number is " +rev);
+       }
+
+}
